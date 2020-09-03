fd5db2d219e73d94487c8465d592766db20926c7
yoonchaiyoung <domo62@naver.com>
2020-09-03 09:53:05 +0900

    web client 연습 #2

--- client/request.py
+++ client/request.py
@@ -1,6 +1,6 @@
-from urllib.request import urlopen
-
-
+from urllib import request
+from urllib.parse import urlencode
+from urllib.request import urlopen, Request
 
 # GET
 http_response = urlopen('https://www.example.com')
@@ -8,10 +8,39 @@ body = http_response.read()
 html = body.decode("utf-8")
 print(html)
 
-
-http_response = urlopne('https://movie.naver.com/movie/sdb/rank/rmovie.nhn')
+http_response = urlopen('https://movie.naver.com/movie/sdb/rank/rmovie.nhn')
 body = http_response.read()
 html = body.decode("cp949")
 print(html)
 
+from urllib.request import urlopen
 
+http_response = urlopen('https://www.example.com?a=10&b=20')  # GET 방식은 데어터가 url에 달려있음
+body = http_response.read()
+html = body.decode("utf-8")
+print(html)
+
+# POST
+data = {
+    'id': 'kickscar',
+    'pw': '1234'
+}
+
+data = urlencode(data).encode('utf-8')
+
+request('https://nid.naver.com/nidlogin.login', data)
+request.add_header('Content-Type', 'text/html')
+
+urlopen(request)
+
+data = {
+    'id': 'kickscar',
+    'pw': '1234'
+}
+
+data = urlencode(data).encode('utf-8')
+
+request('https://www.example.com', data)
+request.add_header('Content-Type', 'text/html')
+
+urlopen(request)
